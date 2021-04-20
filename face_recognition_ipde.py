2a61d915af7528c3be0b3f0ef187df9cdfd6ea88
eyarazar <73524661+eyarazar@users.noreply.github.com>
2021-04-20 19:29:48 +0300

    update face rec python

--- face_recognition_ipde.py
+++ face_recognition_ipde.py
@@ -2,41 +2,31 @@ import face_recognition
 import argparse
 
 
+def face_rec(refference_img, data)
+    #refference_img = face_recognition.load_image_file(path_refference_img)
+    refference_face_encoding = face_recognition.face_encodings(refference_img)[0]
 
-parser = argparse.ArgumentParser(description='Get two images.')
-parser.add_argument('--img_known', type=str,
-                    help='Path for known person img')
-parser.add_argument('--img_unknown', type=str,
-                    help='Path for unknown person img')
+    #face_locations = face_recognition.face_locations(refference_img)
+    #print("I found {} face(s) in this photograph.".format(len(face_locations)))
 
-args = vars(parser.parse_args())
+    # my_face_encoding now contains a universal 'encoding' of my facial features that can be compared to any other picture of a face!
 
-path_refference_img = args["img_known"]
-path_new_img = args["img_unknown"]
+    #new_img = face_recognition.load_image_file(path_new_img)
+    new_face_encoding = face_recognition.face_encodings(data)[0]
 
+    face_locations = face_recognition.face_locations(new_img)
+    #print("I found {} face(s) in this photograph.".format(len(face_locations)))
 
-refference_img = face_recognition.load_image_file(path_refference_img)
-refference_face_encoding = face_recognition.face_encodings(refference_img)[0]
+    # Now we can see the two face encodings are of the same person with `compare_faces`!
 
-face_locations = face_recognition.face_locations(refference_img)
-print("I found {} face(s) in this photograph.".format(len(face_locations)))
+    results = face_recognition.compare_faces([refference_face_encoding], new_face_encoding)
 
-# my_face_encoding now contains a universal 'encoding' of my facial features that can be compared to any other picture of a face!
+    if results[0] == True:
+	return True  # Disarm
+        print("It's a picture of me!")
+    else:
+        return False # Arm
+        print("It's not a picture of me!")
 
-new_img = face_recognition.load_image_file(path_new_img)
-new_face_encoding = face_recognition.face_encodings(new_img)[0]
-
-face_locations = face_recognition.face_locations(new_img)
-print("I found {} face(s) in this photograph.".format(len(face_locations)))
-
-# Now we can see the two face encodings are of the same person with `compare_faces`!
-
-results = face_recognition.compare_faces([refference_face_encoding], new_face_encoding)
-
-if results[0] == True:
-    print("It's a picture of me!")
-else:
-    print("It's not a picture of me!")
-
-face_distance = face_recognition.api.face_distance([refference_face_encoding], new_face_encoding)
-print("Face distance: {}".format(face_distance))
+    #face_distance = face_recognition.api.face_distance([refference_face_encoding], new_face_encoding)
+    #print("Face distance: {}".format(face_distance))
